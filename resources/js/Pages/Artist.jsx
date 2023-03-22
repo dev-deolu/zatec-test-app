6f0a1f28d2ef3e3b153aae5e3528207466723dc3
Adeolu <osunsamiadeolu@icloud.com>
2023-03-22 03:20:16 -0700

    bug fix

--- resources/js/Pages/Artist.jsx
+++ resources/js/Pages/Artist.jsx
@@ -20,7 +20,7 @@ export default function Artist(props) {
                     <h4 className="font-bold text-lg md:text-xl xl:text-3xl">Artists</h4>
                 </div>
                 <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3  xl:grid-cols-5">
-                    {artists.map((artist) => (
+                    {artists && artists.map((artist) => (
                         <ArtistCard
                             key={artist.mbid ? artist.mbid : artist.name}
                             name={artist.name}
