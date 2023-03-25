9f147689da3f270bb3a22f254b38a2ae9d194d9e
Adeolu <osunsamiadeolu@icloud.com>
2023-03-25 11:01:26 -0700

    test: signup

--- tests/Feature/SignUpTest.php
+++ tests/Feature/SignUpTest.php
@@ -7,6 +7,7 @@ use Tests\TestCase;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Laravel\Socialite\Facades\Socialite;
+use Inertia\Testing\AssertableInertia as Assert;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SignUpTest extends TestCase
@@ -15,13 +16,15 @@ class SignUpTest extends TestCase
 
     public function test_signup_screen_can_be_rendered(): void
     {
-        $response = $this->get('/signup');
-
-        $response->assertStatus(200);
+        $this->get('/signup')->assertStatus(200)->assertInertia(
+            fn (Assert $page) => $page
+                ->component('SignUp')
+        );
     }
 
     public function test_new_users_can_signup_using_form(): void
     {
+        $this->test_signup_screen_can_be_rendered();
         $response = $this->post('/signup', [
             'name' => 'Test User',
             'email' => 'test@example.com',
@@ -33,11 +36,25 @@ class SignUpTest extends TestCase
         $response->assertRedirect(RouteServiceProvider::HOME);
     }
 
+    public function test_new_users_can_not_signup_using_unconfimed_password(): void
+    {
+        $this->test_signup_screen_can_be_rendered();
+        $response = $this->post('/signup', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'wrong-password',
+        ]);
+        $this->assertGuest();
+    }
+
     public function test_new_users_can_signup_using_google(): void
     {
+        $this->test_signup_screen_can_be_rendered();
+        $response = $this->get('/google/login');
+        $response->assertFound();
 
         $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
-
         $abstractUser
             ->shouldReceive('getId')
             ->andReturn(rand())
@@ -51,9 +68,7 @@ class SignUpTest extends TestCase
         $provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
         $provider->shouldReceive('user')->andReturn($abstractUser);
 
-        Socialite::shouldReceive('driver')->with('google')
-            ->andReturn($provider);
-
+        Socialite::shouldReceive('driver')->with('google')->andReturn($provider);
         $response = $this->get('/google/redirect');
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
